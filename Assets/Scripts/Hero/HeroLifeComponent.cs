2b2b49681baf3f0449e4b2168fc4d5bf3302f127
VRhino <xonirv@gmail.com>
2025-06-25 23:10:13 +0200

    Add hero respawn system

--- Assets/Scripts/Hero/HeroLifeComponent.cs
+++ Assets/Scripts/Hero/HeroLifeComponent.cs
@@ -1,5 +1,6 @@
 using Unity.Entities;
 
+
 /// <summary>
 /// Tracks the alive/dead state of the hero.
 /// </summary>
@@ -7,4 +8,10 @@ public struct HeroLifeComponent : IComponentData
 {
     /// <summary>True if the hero can move and interact.</summary>
     public bool isAlive;
+
+    /// <summary>Countdown timer while the hero is dead.</summary>
+    public float deathTimer;
+
+    /// <summary>Total time before the hero respawns.</summary>
+    public float respawnDelay;
 }
