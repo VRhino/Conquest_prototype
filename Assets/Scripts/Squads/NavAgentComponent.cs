3729bab92b619afa1f64daa9c1da2d1b9b710eda
VRhino <xonirv@gmail.com>
2025-06-25 23:17:04 +0200

    Add squad navigation system

---
+++ Assets/Scripts/Squads/NavAgentComponent.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+
+/// <summary>
+/// Tag component added to entities that have a <see cref="UnityEngine.AI.NavMeshAgent"/>
+/// attached through conversion.
+/// </summary>
+public struct NavAgentComponent : IComponentData
+{
+}
---
+++ Assets/Scripts/Squads/NavMeshAgentAuthoring.cs
@@ -0,0 +1,22 @@
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Baking component that exposes a <see cref="NavMeshAgent"/> on the GameObject
+/// and links it to an entity via <see cref="NavAgentComponent"/>.
+/// </summary>
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshAgentAuthoring : MonoBehaviour
+{
+    class Baker : Unity.Entities.Baker<NavMeshAgentAuthoring>
+    {
+        public override void Bake(NavMeshAgentAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            var agent = authoring.GetComponent<NavMeshAgent>();
+            AddComponentObject(entity, agent);
+            AddComponent<NavAgentComponent>(entity);
+        }
+    }
+}
