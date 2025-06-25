7e79aa95014fdf08bae784c2c204561193b5558f
VRhino <xonirv@gmail.com>
2025-06-26 00:10:20 +0200

    Add UIManager singleton and system

---
+++ Assets/Scripts/UI/UIManagerSystem.cs
@@ -0,0 +1,31 @@
+using Unity.Entities;
+
+/// <summary>
+/// Listens for changes to <see cref="GameStateComponent"/> and updates
+/// the <see cref="UIManager"/> accordingly.
+/// </summary>
+[UpdateInGroup(typeof(PresentationSystemGroup))]
+public partial class UIManagerSystem : SystemBase
+{
+    GamePhase _lastPhase;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        if (SystemAPI.TryGetSingleton<GameStateComponent>(out var state))
+            _lastPhase = state.currentPhase;
+    }
+
+    protected override void OnUpdate()
+    {
+        if (!SystemAPI.TryGetSingleton<GameStateComponent>(out var state))
+            return;
+
+        if (state.currentPhase != _lastPhase)
+        {
+            _lastPhase = state.currentPhase;
+            if (UIManager.Instance != null)
+                UIManager.Instance.ShowScreen(state.currentPhase);
+        }
+    }
+}
